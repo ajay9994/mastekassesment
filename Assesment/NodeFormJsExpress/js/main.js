be90c15476ac7bc64f3846a6dcc9f5b2f5144b26
ajay9994 <53565330+ajay9994@users.noreply.github.com>
2021-10-06 12:01:04 +0530

    Create main.js

---
+++ Assesment/NodeFormJsExpress/js/main.js
@@ -0,0 +1,24 @@
+function formSubmit(){
+    var firstName=document.getElementById("firstName").value;
+    var lastName=document.getElementById("lastName").value;
+    
+    if(firstName==''){
+        alert("Please fill the firstName");
+        return false;
+    }else if(firstName.length<=2){
+        alert("Firstname should be greater than 2 letter")
+        return false;
+    }else if(lastName==''){
+        alert("Please fill the lastName");
+        return false;
+    }else if(lastName.length<=2){
+        alert("lastName should be greater than 2 letter")
+        return false;
+    }
+    // }else{
+    //     alert("Submitted");
+    // }
+
+    
+
+}
