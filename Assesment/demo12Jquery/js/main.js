8334eff5bda223a8a295c814b5033fb668806057
ajay9994 <53565330+ajay9994@users.noreply.github.com>
2021-10-06 11:47:13 +0530

    Create main.js

---
+++ Assesment/demo12Jquery/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    const root = $("#root");
+    root.append("<ul id='list'></ul>");
+
+    $("#loadJsonArray").click(function(){
+        const xmlhttp = new XMLHttpRequest();
+        xmlhttp.onload = function () {
+            const people = JSON.parse(this.responseText);
+            const list=people.people;
+            for(i in list ){
+                $('#list').append("<li>"+list[i].firstName+" "+list[i].lastName+"</li>");
+            }
+          }
+          xmlhttp.open("GET", "people.json");
+          xmlhttp.send();
+    })
+
+})
